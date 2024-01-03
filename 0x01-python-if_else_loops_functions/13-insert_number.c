637d1e2fb1257bcd1a08603959a5c5b5e16ef5a4
FEB-IRI <felixfebiri343@gmail.com>
2024-01-03 13:18:22 -0800

    done

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -40,35 +40,3 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
     new = malloc(sizeof(listint_t));
     if (new == NULL)
         return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
-
-    return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
-}
