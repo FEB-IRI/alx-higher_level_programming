27e91ed08005c09899fc1e3dbef3429e9867494f
FEB-IRI <felixfebiri343@gmail.com>
2024-01-03 13:41:52 -0800

    done

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
@@ -10,13 +9,12 @@
 size_t print_listint(const listint_t *h)
 {
     const listint_t *current;
-    unsigned int n; /* number of nodes */
+    unsigned int n = 0; /* number of nodes */
 
     current = h;
-    n = 0;
     while (current != NULL)
     {
-        printf("%i\n", current->n);
+        printf("%d\n", current->n);
         current = current->next;
         n++;
     }
@@ -35,8 +33,25 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
     listint_t *new;
     listint_t *current;
 
-    current = *head;
-
     new = malloc(sizeof(listint_t));
     if (new == NULL)
         return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = new;
+        return (new);
+    }
+
+    current = *head;
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = new;
+
+    return (new);
+}
